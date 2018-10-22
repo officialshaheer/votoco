cd867c32704fa2210c19bb8839f2716d1a4a2132
Shaheer S S <38754443+officialshaheer@users.noreply.github.com>
2018-10-22 01:58:29 -0700

    Added Candidate Information Insertion Queries
    Database connection and candidate adding

---
+++ process/addCandidateToDB.php
@@ -0,0 +1,37 @@
+<?php 
+	
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "votoco";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+	$c_id = $_POST['c_id'];
+	$c_name = $_POST['candidatename'];
+	$c_department = $_POST['c_department'];
+	$c_position = $_POST['c_position'];
+
+	// if($c_id==" "||$c_name==""||$c_department=""||$c_position=="") {
+	// 	echo "Null!"
+	// }
+
+	if( isset( $_POST['c_id'] ) ) {
+
+		$sql1 = "SELECT * from `candidates` where c_id = '$c_id' ";
+		mysqli_query($conn,$sql1);
+			if ($conn->query($sql1)->num_rows) {
+			echo "Already Entered!";
+			}
+			else {
+			$sql = "INSERT into `candidates` (c_id,c_name,c_department,position) VALUES ('$c_id','$c_name','$c_department','$c_position')"; 
+			mysqli_query($conn,$sql);
+				if(mysqli_query($conn,$sql)) {
+				echo "entered the candidate!";
+				}
+				else {
+				echo "Not Entered in Database";
+				}
+			}
+		}
+?>	
