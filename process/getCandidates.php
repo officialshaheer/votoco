e9627be1f3ecd81551c06dbb871cbb86c5502442
gouthamsivakumar1 <gouthamsivakumar1@gmail.com>
2018-10-26 03:02:34 -0700

    getCandidate page uploaded
    displaying of candidates in voting page.

---
+++ process/getCandidates.php
@@ -0,0 +1,24 @@
+<?php 
+	
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "votoco";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    		$position = $_POST['position'];
+	
+	if( isset( $_POST['position'] ) ) {
+
+		$sql = "SELECT * from `candidates` where position = '$position' "; 
+		if ($conn->query($sql)->num_rows) {
+		echo '<input type="text" value="POSITION IS DISPLAYED">';
+		}
+		else
+		{
+		echo 'Not executed';
+		}
+	}
+		
+?>
