b39f116d8ae41a2954fbb7fbfa1ccd3921ba8a12
Shaheer S S <38754443+officialshaheer@users.noreply.github.com>
2018-11-14 14:32:04 +0530

    This will destroy the voters id from the session.

--- logout.php
+++ logout.php
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
 <?php 
-	
+
 	session_start();
 	$_SESSION['voters_id'] = "";
-	session_destroy();
 
 	header('Location: vote.php');
-=======
-<?php 
-	
-	session_start();
-	$_SESSION['voters_id'] = "";
-	session_destroy();
-
-	header('Location: vote.php');
->>>>>>> 50712c5a151373f2f775ad87eabddb8355052a3b
  ?>
