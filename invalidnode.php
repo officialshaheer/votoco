a6ca461f75d8a47c5d28e14a7b787677030fd50a
ajayjudeunni <44311956+ajayjudeunni@users.noreply.github.com>
2018-11-14 14:26:36 +0530

    Landing page : User interface for an invalid node
    The user will be redirected to this 'invalidnode.php' if the 'system hash' session is destroyed

---
+++ invalidnode.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Invalid Node - VotoCo</title>
+</head>
+<body style="background-color: red;">
+
+	<center><a href="nodelogin.php">Log in to Node!</a><p style="font-size: 100px;color: white;">THIS IS NOT AN AUTHORIZED NODE TO VOTE.</p></center>
+
+
+</body>
+</html>
