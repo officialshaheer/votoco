2c46c4f10a0b34c02045f9d3fff2f1beac5b4f9e
Shaheer <officialshaheer@gmail.com>
2018-11-14 15:31:43 +0530

    User Interface for Retrieving Nodes
    User Interface for Retrieving Nodes & Allocating nodes
    
    Co-Authored-By: ajayjudeunni <ajayjudeunni@users.noreply.github.com>
    Co-Authored-By: gouthamsivakumar1 <gouthamsivakumar1@users.noreply.github.com>

--- votoco.php
+++ votoco.php
@@ -40,7 +40,7 @@ input[type="button"]:hover {
 .answer_list {
   display:none;
   width: 700px;
-  height: 330px;
+  height: 530px;
   position: fixed;
   top:25%;
   left:30%;
@@ -50,6 +50,28 @@ input[type="button"]:hover {
 </style>
 </head>
 <body>
+
+<!-- <?php
+
+// $servername = "localhost";
+// $username   = "root";
+// $password   = "";
+
+// $conn = new mysqli($servername, $username, $password);
+// $sql = 'CREATE DATABASE votoco1';
+// if ($conn->query($sql)) {
+    // echo "Database my_db created successfully\n";
+   // $sql2 = "CREATE table votoco1.users(id varchar(2))";
+    // if ($conn->query($sql2)) {
+        // echo "doneee";
+    // } else {
+        // echo "error ";
+    // }
+// } else {
+    // echo 'Error creating database: ' . mysql_error() . "\n";
+// }
+?> -->
+
 
 <div class="limiter">
    <div class="content-new">
@@ -61,6 +83,18 @@ input[type="button"]:hover {
             <div id="sysAllocation"  style="background-color:black;" class="answer_list" >
                <br>
                <h2> List of Nodes!</h2>
+
+                <label style="color:white;">Nodes Available :</label>
+                 <div class="retrieveNodes"></div><br>
+                 <input id="allocateSystemBtn" type="button" name="AddCandidate" value="Allocate System">
+                 <div class="giveUrl"></div><br>
+                <label style="color:white;">Active Nodes :</label>
+                <div class="activeNodes"></div>
+                <a style="color: white;" href="resetNodes.php">DESTROY SESSION</a>
+        
+
+
+
             </div>
             <div id="AddCandidate"  style="background-color:green;" class="answer_list">
                <br>
@@ -69,23 +103,23 @@ input[type="button"]:hover {
                  <table>
                   <tr>
                     <td>
-                 <label>Candidate ID :</label></td><td><input type="text" id="c_id"><div style="color: white;" class="tikholder"></div>
+                 <label style="color:white;">Candidate ID :</label></td><td><input type="text" id="c_id"><div style="color: white;" class="tikholder"></div>
                     </td>
                   </tr>
                   
                   <tr>
                     <td>
-                 <label>Candidate Name :</label></td><td><input type="text" id="candidatename"><br>
+                 <label style="color:white;">Candidate Name :</label></td><td><input type="text" id="candidatename"><br>
                     </td>
                   </tr>
                   <tr>
                     <td>
-                 <label>Candidate Department :</label></td><td><input type="text" id="can_department"><br>
+                 <label style="color:white;">Candidate Department :</label></td><td><input type="text" id="can_department"><br>
                     </td>
                   </tr>
                   <tr>
                     <td>  
-                 <label>Position :</label></td><td><input type="text" id="can_position"><br>
+                 <label style="color:white;">Position :</label></td><td><input type="text" id="can_position"><br>
                     </td>
                   </tr>
                   <tr>
@@ -104,10 +138,50 @@ input[type="button"]:hover {
             <div id="ProfileUpdate"  style="background-color:blue;" class="answer_list" >
                <br>
                <h2> Update Candidate Profile</h2>
+
+               <form id="updateForm">
+                 <table>
+                  <tr>
+                    <td>
+                 <label style="color:white;">Candidate ID :</label></td><td>
+                    
+                    <div class="retrieveCandidates"></div>
+                    
+                    <div style="color: white;" class="tikholder"></div>
+                    </td>
+                  </tr>
+                  
+                  <tr>
+                    <td>
+                 <label style="color:white;">Candidate Name :</label></td><td><input type="text" id="UPcandidatename"><br>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                 <label style="color:white;">Candidate Department :</label></td><td><input type="text" id="UPcan_department"><br>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>  
+                 <label style="color:white;">Position :</label></td><td><input type="text" id="UPcan_position"><br>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>  </td><td>
+                      <input id="updateBtn" type="button" name="AddCandidate" value="Update Candidate">
+                    </td>
+                  </tr> 
+                </table>   
+               </form>
+
             </div>
             <div id="GrantRevoke"  style="background-color:green;" class="answer_list" >
                <br>
                <h2> Voters - Grant Revoke</h2>
+               <div class="retrieveVoters"></div>
+               <br>
+               <input id="GrantBtn" type="button" name="" value="GRANT">
+               <input id="RevokeBtn" type="button" name="" value="REVOKE">
             </div>
             <input id="myButton" type="button" name="login" value="System Allocation" onclick="showBox('sysAllocation')"><br><br>
             <input type="button" name="login" value="Adding Candidate" onclick="showBox('AddCandidate')"><br><br>
@@ -150,9 +224,10 @@ input[type="button"]:hover {
 
 <script type="text/javascript">
    $(function(){
+
+      // Adding Candidate fn1
       $('#addCandidateBtn').click(function(event){
 
-                // event.preventDefault();
                 var c_id = $('#c_id').val();
                 var c_name = $('#candidatename').val();     
                 var c_department = $('#can_department').val();
@@ -174,8 +249,167 @@ input[type="button"]:hover {
                     }
                   })
             });
+      // fn1 ends
+
+      //Retrieving Candidate list 
+
+      $(document).ready(function() {
+
+                var c_id = $('#c_id').val();
+                var c_name = $('#candidatename').val();     
+                var c_department = $('#can_department').val();
+                var c_position = $('#can_position').val();
+
+                $.ajax({
+                    url: 'process/retrievingCandidate.php',
+                    type: 'POST',
+                    data: {
+                        c_id: c_id,
+                        candidatename: c_name,
+                        c_department: c_department,
+                        c_position:c_position
+                      },
+                success: function(data){
+                      // alert(data);
+                      // console.log(data);
+                      $('.retrieveCandidates').append(data);                    
+                    }
+                  })
+            });
+
+      //Updating Candidate List
+      $('#updateBtn').click(function(event){
+
+                var c_id = $('#cndList').val();
+                var c_name = $('#UPcandidatename').val();     
+                var c_department = $('#UPcan_department').val();
+                var c_position = $('#UPcan_position').val();
+                
+                $.ajax({
+                    url: 'process/updateCandidateToDB.php',
+                    type: 'POST',
+                    data: {
+                        c_id: c_id,
+                        candidatename: c_name,
+                        c_department: c_department,
+                        c_position:c_position
+                      },
+                success: function(data){
+                      alert(data);
+                      // console.log(data);
+                      // $('.updateCandidate').append(data);                    
+                    }
+                  })
+            });
+
+      //Retrieving VotersList for Granting Permission
+            $(document).ready(function() {
+
+               $.ajax({
+                    url: 'process/retrievingVoters.php',
+                    type: 'GET',
+                    
+                success: function(data){
+                      // alert(data);
+                      // console.log(data);
+                      $('.retrieveVoters').html(data);                    
+                    }
+                  })
+            });
+
+      //GRANTING & REVOKING A USER 
+
+      $('#GrantBtn').click(function(event){
+
+                var voters_id = $('#votersList').val();
+                var btnId = $(this).val();
+
+                $.ajax({
+                    url: 'process/GrantRevoke.php',
+                    type: 'POST',
+                    data: {
+                        voters_id: voters_id,
+                        btnId: btnId
+                      },
+                success: function(data){
+                      alert(data);
+                      // console.log(data);
+                      // $('.updateCandidate').append(data);                    
+                    }
+                  })
+            });
+
+      $('#RevokeBtn').click(function(event){
+
+                var voters_id = $('#votersList').val();
+                var btnId = $(this).val();
+                
+                $.ajax({
+                    url: 'process/GrantRevoke.php',
+                    type: 'POST',
+                    data: {
+                        voters_id: voters_id,
+                        btnId: btnId
+                      },
+                success: function(data){
+                      alert(data);
+                      // console.log(data);
+                      // $('.updateCandidate').append(data);                    
+                    }
+                  })
+            });
+
+      //Retrieving Nodes list for Granting Permission
+            $(document).ready(function() {
+
+               $.ajax({
+                    url: 'process/retrievingAvailableNodes.php',
+                    type: 'GET',
+                    
+                success: function(data){
+                      // alert(data);
+                      // console.log(data);
+                      $('.retrieveNodes').html(data);                    
+                    }
+                  })
+            });
+
+      $('#allocateSystemBtn').click(function(event){
+
+                var node_id = $('#nodesList').val();
+                
+                
+                $.ajax({
+                    url: 'process/allocateSystem.php',
+                    type: 'POST',
+                    data: {
+                        node_id: node_id,
+                        },
+                success: function(data){
+                      // alert(data);
+                      // console.log(data);
+                      $('.giveUrl').html(data);                    
+                    }
+                  })
+            });
+
 
    });
+  </script>
+
+  <script type="text/javascript">
+
+    $(document).ready(function() {
+          $.ajax({
+              url: 'process/activeNodesCheck.php',
+              type: 'GET',
+              success: function(data){
+                $('.activeNodes').html(data);     
+                }
+          });  
+        });
+        
+
   </script>
 
 
